2e4cdd362e219bf7106c5b76387db63770929656
Sergio Andres Muñoz Pinillos <pinilloss@CAMPUSUNINORTE.LOCAL>
2019-04-05 13:52:39 -0500

    floyd

--- Floydwarshall/src/floydwarshall/Floydwarshall.java
+++ Floydwarshall/src/floydwarshall/Floydwarshall.java
@@ -5,17 +5,61 @@
  */
 package floydwarshall;
 
-/**
- *
- * @author pinilloss
- */
+
 public class Floydwarshall {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
     
+    public static void main(String[] args) {
+        int grafo[][]=
+        {
+            {0,0,0,2,0},
+            {3,0,0,0,0},
+            {9,0,0,5,8},
+            {0,1,0,0,0},
+            {2,0,0,2,0},
+        };
+    }
+    static void floyd(int grafo[][]){
+            int n=grafo.length;
+            int distancia[][]=getMatriz(0, grafo);
+            int caminos[][]=getMatriz(1, grafo);  
+            int tmp;
+            for (int k = 0; k < n; k++) {
+                for (int i = 0; i < n; i++) {
+                    for (int j = 0; j < n; j++) {
+                        tmp= distancia[i][k]+distancia[k][j];
+                        if (tmp<distancia[i][j]) {
+                            distancia[i][j]=tmp;
+                            caminos[i][j]=k;
+                        }
+                    }
+                }
+        }
+        }
+    static void print(String txt, int M[][]){
+        
+    }
+    static int[][] getMatriz(int tipo, int grafo[][]){
+        int n=grafo.length;
+        int M[][]= new int[n][n];
+        if (tipo==1) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    M[i][j]=j;
+                }
+            }
+        }else{
+            int INF=999;
+            for (int i = 0; i < 10; i++) {
+                for (int j = 0; j < 10; j++) {
+                    if (i!=j && grafo[i][j]==0) {
+                        M[i][j]=INF;
+                    }else{
+                        M[i][j]=grafo[i][j];
+                    }
+                }
+            }
+        }
+        return M;
+    }
 }
